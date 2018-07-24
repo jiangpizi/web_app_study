1f1d56ee21daef3350d50661dc46b74f811adf79
jsong <601996910@qq.com>
2018-07-24 08:13:24 +0800

    script

---
+++ script/db_rest.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+WORKDIR=`pwd`
+
+# start mysql and create database
+#service mysqld start
+#mysql -uroot << EOF
+#drop database poc_db;
+#drop user 'poc_user'@'localhost';
+#flush privileges;
+#EOF
+mysql -u root -p << EOF
+create user 'web_user'@'localhost' identified by 'web_pwd';
+create database web_db default character set = utf8;
+grant all on web_db.* to 'web_user'@'localhost';
+EOF
+echo "web_db  created ok."
+echo "----> ALL DONE <-------"
+
